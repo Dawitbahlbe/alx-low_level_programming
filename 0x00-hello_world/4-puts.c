50a27187250e61445dc6a62a4d2b3b26f96fdf82
Dawitbahlbe <dbahlbe8@gmail.com>
2022-06-04 09:32:08 -0700

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,9 @@
 #include<stdio.h>
-/**
+int main(void)/**
  *  *  * main - Entry point
  *    *    *
- *    *    *Return Always 0 (success)
- *    **/
-int main(void)
+ *    *Return Always 0 (success)**/
 {
-
-    puts("\"Programming is like building a multilingual puzzle");
-    return 0;
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
